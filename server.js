863df602931dd26d5c8ba141da42e26960e6268e
Rabah Berrehal <matteo.berrehal08@gmail.com>
2026-02-17 15:28:33 +0100

    Add environment configuration and module exports for various packages

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-import http from "node:http";
-import { json } from "./outils/responses.js";
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
+  res.send({
     message: "users management api",
   });
 });
+const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
   console.log("Server running at http://127.0.0.1:3000/");
