72e2f50ebefc434f0fc58448395ef143b703394f
Rabah Berrehal <matteo.berrehal08@gmail.com>
2026-02-17 12:30:12 +0100

    Add initial implementation of utility functions and ESLint configurations

---
+++ index.js
@@ -0,0 +1,10 @@
+function fullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+function helloworld() {
+  let name = fullName("berre", "rabahgit branch -M main");
+  console.log("hello world " + name);
+}
+
+helloworld();
